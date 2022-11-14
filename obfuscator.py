06c7a2e8c9f7f4637aec6a34acbaa168b9e7c5ae
notnewuser1337 <116939753+notnewuser1337@users.noreply.github.com>
2022-11-14 01:31:08 +0100

    Update obfuscator.py
    no need for admin permissions now, obfuscated file comes out as (obfuscated).py

--- obfuscator.py
+++ obfuscator.py
@@ -2,29 +2,36 @@ import os
 import base64
 from sys import argv
 
-randomizer = 100
-obu_text = '_allahuakbar_123_1337_7331' * 100
+OFFSET = 10
+VARIABLE_NAME = '__NO_NO' * 100
 
 def obfuscate(content):
     b64_content = base64.b64encode(content.encode()).decode()
     index = 0
-    code = f'{obu_text} = ""\n'
-    for _ in range(int(len(b64_content) / randomizer) + 1):
+    code = f'{VARIABLE_NAME} = ""\n'
+    for _ in range(int(len(b64_content) / OFFSET) + 1):
         _str = ''
-        for char in b64_content[index:index + obu_text]:
+        for char in b64_content[index:index + OFFSET]:
             byte = str(hex(ord(char)))[2:]
             if len(byte) < 2:
                 byte = '0' + byte
             _str += '\\x' + str(byte)
-        code += f'{obu_text} += "{_str}"\n'
-        index += randomizer
-    code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode{obu_text}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
+        code += f'{VARIABLE_NAME} += "{_str}"\n'
+        index += OFFSET
+    code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode({VARIABLE_NAME}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
     return code
 
 def main():
 
     try:
         path = argv[1]
+        if not os.path.exists(path):
+            print("error")
+            exit()
+
+        if not os.path.isfile(path) or not path.endswith('.py'):
+            print("error")
+            exit()
         
         with open(path, 'r', encoding='utf-8', errors='ignore') as file:
             file_content = file.read()
@@ -34,18 +41,8 @@ def main():
         with open(f'{path.split(".")[0]} (obfuscated).py', 'w') as file:
             file.write(obfuscated_content)
 
-        print('[+] obfuscated' + os.file.name )
-
-    except: 
-        
-         if not os.path.isfile(path) or not path.endswith('.py'):
-            print('Error, not a python file')
-         
-         else: 
-
-            if not os.path.exists(path):
-                print('Error, file not found')
-            
+    except:
+        print(f' python {argv[0]} filename.py')
 
 if __name__ == '__main__':
     main()
